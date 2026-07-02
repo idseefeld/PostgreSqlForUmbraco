e7a1e1240077752d8465d06f043ed889c7ff5303
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2026-07-02 13:41:54 +0200

    add provider for Umbraco.Commerce

---
+++ Our.Umbraco.PostgreSql/Our.Umbraco.PostgreSql.Commerce/PostgreSqlEFCoreComposer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Our.Umbraco.PostgreSql.Commerce
+{
+    public class PostgreSqlEFCoreComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            
+        }
+    }
+}
